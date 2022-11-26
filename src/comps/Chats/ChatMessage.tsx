ec8d7f536e4692be0be301dc56fa65e816e618c9
Eugeny Silyaev <esilayev@gmail.com>
2022-11-27 00:27:50 +0300

    simplify logic for render message

--- src/comps/Chats/ChatMessage.tsx
+++ src/comps/Chats/ChatMessage.tsx
@@ -3,47 +3,40 @@ import { useColorModeValue } from "@chakra-ui/color-mode";
 import { ListItem, Flex, ListIcon, Text } from "@chakra-ui/layout";
 import { HiChevronDown, HiChevronUp } from "react-icons/hi";
 import { Message } from "../../store/messageSlice";
+import { IconType } from "react-icons/lib";
 
 
 
-const ChatMessage = (props: Message) => {
+const ChatMessage = ({ message, type }: Message) => {
   const messageBg = useColorModeValue("white", "gray.600");
-  switch (props.type) {
-    case "inc":
-      return (
-        <ListItem>
-          <Flex
-            justify={"end"}
-            alignItems={"center"}
-            bg={messageBg}
-            borderRadius="lg"
-            p="4"
-            m="2"
-          >
-            <Text>{props.message}</Text>
-            <ListIcon as={HiChevronDown} color="green.500" mx={2} />
-          </Flex>
-        </ListItem>
-      );
-    case "out":
-      return (
-        <ListItem>
-          <Flex
-            justify={"start"}
-            alignItems={"center"}
-            bg={messageBg}
-            borderRadius="lg"
-            p="4"
-            m="2"
-          >
-            <ListIcon as={HiChevronUp} color="blue.500" mx={2} />
-            <Text>{props.message}</Text>
-          </Flex>
-        </ListItem>
-      );
-    default:
-      return <h2>Something wrong !</h2>;
+  let justify: string
+  let icon: IconType
+  switch (type) {
+    case 'inc':
+      justify = "end"
+      icon = HiChevronDown
+      break
+    case 'out':
+      justify = "start"
+      icon = HiChevronUp
+      break
   }
+
+  return (
+    <ListItem>
+      <Flex
+        justify={justify}
+        alignItems={"center"}
+        bg={messageBg}
+        borderRadius="lg"
+        p="4"
+        m="2"
+      >
+        <Text>{message}</Text>
+        <ListIcon as={icon} color="green.500" mx={2} />
+      </Flex>
+    </ListItem>
+  )
 }
 
 export default ChatMessage
