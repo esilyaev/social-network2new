2d4c2c95e80479fbae2a1ac782679e789fea6c07
Eugeny Silyaev <esilayev@gmail.com>
2022-11-27 00:38:37 +0300

    no switch in chat message comp

--- src/comps/Chats/ChatMessage.tsx
+++ src/comps/Chats/ChatMessage.tsx
@@ -3,32 +3,27 @@ import { useColorModeValue } from "@chakra-ui/color-mode";
 import { ListItem, Flex, ListIcon, Text } from "@chakra-ui/layout";
 import { HiChevronDown, HiChevronUp } from "react-icons/hi";
 import { Message } from "../../store/messageSlice";
-import { IconType } from "react-icons/lib";
 
+const IncOption = {
+  justify: "end",
+  icon: HiChevronDown,
+  color: 'green.500',
+}
+const OutOption = {
+  justify: "start",
+  icon: HiChevronUp,
+  color: 'blue.500',
+}
 
 
 const ChatMessage = ({ message, type }: Message) => {
   const messageBg = useColorModeValue("white", "gray.600");
-  let justify: string
-  let icon: IconType
-  let color: string
-  switch (type) {
-    case 'inc':
-      justify = "end"
-      icon = HiChevronDown
-      color = 'green.500'
-      break
-    case 'out':
-      justify = "start"
-      icon = HiChevronUp
-      color = 'blue.500'
-      break
-  }
+  const option = type === 'inc' ? IncOption : OutOption
 
   return (
     <ListItem>
       <Flex
-        justify={justify}
+        justify={option.justify}
         alignItems={"center"}
         bg={messageBg}
         borderRadius="lg"
@@ -36,7 +31,7 @@ const ChatMessage = ({ message, type }: Message) => {
         m="2"
       >
         <Text>{message}</Text>
-        <ListIcon as={icon} color={color} mx={2} />
+        <ListIcon as={option.icon} color={option.color} mx={2} />
       </Flex>
     </ListItem>
   )
